eae206d078e0cc07b873817b5e3e705a78324962
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-10-01 19:40:43 +0530

    WIP

--- snippet/components.tsx
+++ snippet/components.tsx
@@ -1,5 +1,8 @@
+import React from "react";
+import { makeSnippet, $0 } from "./config";
+
 makeSnippet(
-  ({ Avatar }) => <Avatar title="$#" __options={["size", "rounded"]} $ />,
+  ({ Avatar }) => <Avatar title="$0" __options={["size", "rounded"]} $ />,
   "rneAvatar"
 );
 
@@ -8,7 +11,7 @@ makeSnippet(
     <>
       <Avatar
         source={{
-          uri: "$#",
+          uri: "$0",
         }}
         __options={["size"]}
         $
@@ -23,10 +26,13 @@ makeSnippet(
   "rneBadgeAvatar"
 );
 
-makeSnippet(({ Badge }) => <Badge __options={["status"]} $ />, "rneBadge", "");
+makeSnippet(
+  ({ Badge, Text }) => <Badge __options={["status"]} $ />,
+  "rneBadge"
+);
 
 makeSnippet(
-  ({ Badge }) => <Badge __options={["status"]} value="$#" $ />,
+  ({ Badge }) => <Badge __options={["status"]} value="$0" $ />,
   "rneBadgeValue"
 );
 
@@ -34,10 +40,10 @@ makeSnippet(
   ({ BottomSheet }) => (
     <BottomSheet
       __options={["isVisible", "onBackdropPress"]}
-      containerStyle={{ backgroundColor: "$#" }}
+      containerStyle={{ backgroundColor: "$0" }}
       $
     >
-      $#
+      $0
     </BottomSheet>
   ),
   "rneBottomSheet"
@@ -58,125 +64,155 @@ makeSnippet(
   "rneButtonType"
 );
 
-makeSnippet(({ ButtonGroup }) => (
-  <ButtonGroup __options={["onPress", "selectedIndex", "button"]} $ />
-));
+makeSnippet(
+  ({ ButtonGroup }) => (
+    <ButtonGroup __options={["onPress", "selectedIndex", "button"]} $ />
+  ),
+  "rneButtonGroup"
+);
 
-makeSnippet(({ Card }) => (
-  <Card>
-    <Card.Title>$#</Card.Title>
-    {/* \${#:
+makeSnippet(
+  ({ Card }) => (
+    <Card>
+      <Card.Title>$0</Card.Title>
+      {/* \${#:
 <Card.Divider/>}
-<Card.Image source={$#}>
+<Card.Image source={$0}>
  \${#:
   <Text style={{marginBottom: 10}}>
-  $# 
+  $0 
   </Text>
   }
-      $#
+      $0
 </Card.Image> */}
-  </Card>
-));
-
-makeSnippet(({ CheckBox }) => (
-  <CheckBox
-    title="$#"
-    iconType="\${#${iconType}}"
-    checkedIcon="$#"
-    uncheckedIcon="$#"
-    checkedColor="$#"
-    checked={$0}
-  />
-));
-
-
-makeSnippet(({Chip})=><Chip
-
-    __options={['type','title']}
-/>)
-
-makeSnippet(({Chip})=><Chip
-__options={['type','title']}
-
-    icon={{
-        name: "$#",
-        type: "$#",
-        color: "$#",
-    }}  
-/>)
-
-makeSnippet(({Divider})=><Divider  />)
-
-
-makeSnippet(({FAB,})=><FAB 
-    title="$#" 
-    __options={['placement','size']}
-    $/>
+    </Card>
+  ),
+  "rneCard"
 );
-makeSnippet(({Icon})=>
-    <Icon
-  name="$#"
-  type="\${#${iconType}}"
-  size={$#}
-  __op
-  color="$#"
-  $0
-/>
-)
 
-makeSnippet(({Image})=><Image
-  source={{ uri: '$#' }}
-  style={{ width: $#, height: $# }}
-  // \${#:PlaceholderContent={$#}}
-/>)
+makeSnippet(
+  ({ CheckBox }) => (
+    <CheckBox
+      title="$0"
+      iconType="\${#${iconType}}"
+      checkedIcon="$0"
+      uncheckedIcon="$0"
+      checkedColor="$0"
+      checked={$0}
+    />
+  ),
+  "rneCheckbox"
+);
 
-makeSnippet(({LinearProgress})=><LinearProgress color="\${#${color}}"  variant="determinate"  value={$#} $0/>
-)
+makeSnippet(
+  ({ Chip }) => <Chip __options={["type", "title"]} />,
+  "rneChip",
+  "Chip component"
+);
 
-makeSnippet(({LinearProgress})=><LinearProgress color="\${#${color}}" $0/>
-)
+makeSnippet(
+  ({ Chip }) => (
+    <Chip
+      __options={["type", "title"]}
+      icon={{
+        name: "$0",
+        type: "$0",
+        color: "$0",
+      }}
+    />
+  ),
+  "rneChipIcon"
+);
 
-makeSnippet(({CheckBox})=><CheckBox
-  title='$#'
-  iconType="material"
-  checkedIcon='radio-button-checked'
-  uncheckedIcon='radio-button-unchecked'
-  checkedColor='$#'
-  checked={$#}
-/>)
+makeSnippet(({ Divider }) => <Divider />, "rneDivider");
 
-makeSnippet(()=><SearchBar
-    placeholder="$#"
-    onChangeText={$#}
-    value={$#}
-    $0
-/>
-)makeSnippet(()=>
-<Slider
-    value={$#}
-    onValueChange={$#}
-    maximumValue={$#}
-    minimumValue={$#}
-    thumbProps={{$#}}
-/>
-)makeSnippet(()=>
-<SpeedDial
-    isOpen={$#}
-    onOpen={$#}
-    onClose={$#}
-    icon={{ name: '$#' }}
-    openIcon={{ name: '$#' }}
->
-  $0
-</SpeedDial>
-)makeSnippet(()=>
-<SpeedDial.Action
-    icon={{ name: '$#' }}
-    title="$#"
-    onPress={$#}
-  />
-  )makeSnippet(()=>
+makeSnippet(
+  ({ FAB }) => <FAB __options={["placement", "size", "title"]} $ />,
+  "rneFab"
+);
 
-  <Text \${#${size}} $1styles={{$#}}>
-    $0
-</Text>)
+makeSnippet(
+  ({ Icon }) => <Icon name="$0" __options={["size", "type", "color"]} $ />,
+  "rneIcon"
+);
+
+makeSnippet(
+  ({ Image }) => (
+    <Image
+      source={{ uri: "$0" }}
+      style={{ width: $0, height: $0 }}
+      // \${#:PlaceholderContent={$0}}
+    />
+  ),
+  "rneImage"
+);
+
+makeSnippet(({ LinearProgress }) => (
+  <LinearProgress __options={["color"]} variant="determinate" value={$0} $ />
+));
+
+makeSnippet(
+  ({ LinearProgress }) => <LinearProgress color="\${#${color}}" $ />,
+  "rneLinearProgress"
+);
+
+makeSnippet(
+  ({ CheckBox }) => (
+    <CheckBox
+      title="$0"
+      iconType="material"
+      checkedIcon="radio-button-checked"
+      uncheckedIcon="radio-button-unchecked"
+      checkedColor="$0"
+      checked={$0}
+    />
+  ),
+  "rneCheckBox"
+);
+
+makeSnippet(({ SearchBar }) => (
+  <SearchBar placeholder="$0" onChangeText={$0} value={$0} />
+));
+makeSnippet(
+  ({ Slider }) => (
+    <Slider
+      value={$0}
+      onValueChange={$0}
+      maximumValue={$0}
+      minimumValue={$0}
+      thumbProps={{ $0 }}
+    />
+  ),
+  "rneSlider"
+);
+
+makeSnippet(
+  ({ SpeedDial }) => (
+    <SpeedDial
+      isOpen={$0}
+      onOpen={$0}
+      onClose={$0}
+      icon={{ name: "$0" }}
+      openIcon={{ name: "$0" }}
+    >
+      $0
+    </SpeedDial>
+  ),
+  "rneSpeedDial"
+);
+
+makeSnippet(
+  ({ SpeedDial }) => (
+    <SpeedDial.Action icon={{ name: "$0" }} title="$0" onPress={$0} />
+  ),
+  "rneSpeedDialAction"
+);
+
+makeSnippet(
+  ({ Text }) => (
+    <Text __options={["h1", "h2", "h3"]} $1styles={{ $0 }}>
+      $0
+    </Text>
+  ),
+  "rneText"
+);
